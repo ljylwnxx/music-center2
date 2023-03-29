0d5423f380c9709a09aebcd6c58af6efbccfa267
ljylwnxx <ljylwnxx@163.com>
2023-03-29 18:49:01 +0800

    chore:跨域代理

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
-
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [createVuePlugin()],
+  // define: {
+  //   /** 项目环境变量 **/
+  //   'process.env.BASE_ENV': JSON.stringify(process.env.BASE_ENV)
+  // },
   resolve: {
     alias: [
       {
@@ -12,5 +16,19 @@ export default defineConfig({
       }
     ],
     extensions: [".vue", ".js", ".json"],
+  },
+  server: {
+    host: 'localhost',
+    port: 8080,
+    fs: {
+      strict: false
+    },
+    proxy: {
+      '^/api':{
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    },
   }
 })
